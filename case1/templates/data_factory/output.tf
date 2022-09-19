c793bd2f173b39319ffc2a4a9d4cb3b64d735b8e
Carlos Brito Leal <cbritole@emeal.nttdata.com>
2022-09-19 15:37:49 +0100

    fixes small tweaks 1.0.6

--- case1/templates/data_factory/output.tf
+++ case1/templates/data_factory/output.tf
@@ -2,10 +2,6 @@ output "output_vault" {
   value = {}
 
   depends_on = [
-<<<<<<< HEAD
-    azurerm_data_factory.DV
-=======
     azurerm_data_factory.DF
->>>>>>> b188a8b06181aee328c95c367d0bf3f210948040
   ]
 }

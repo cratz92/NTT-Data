b188a8b06181aee328c95c367d0bf3f210948040
Marcos Silvino <marcospaulo.silvino@gmail.com>
2022-09-19 15:30:41 +0100

    update data_factory.tf

--- case1/templates/data_factory/main.tf
+++ case1/templates/data_factory/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_data_factory" "DV" {
+resource "azurerm_data_factory" "DF" {
   name                = var.data_name
   location            = var.location_factory
   resource_group_name = var.resource_group_name
@@ -7,13 +7,13 @@ resource "azurerm_data_factory" "DV" {
 
 resource "azurerm_data_factory_linked_service_key_vault" "linked_keyvault" {
   name            = var.linked_name
-  data_factory_id = azurerm_data_factory.DV.id
-  key_vault_id    = data.azurerm_key_vault.example.id
+  data_factory_id = azurerm_data_factory.DF.id
+  key_vault_id    = data.azurerm_key_vault.DV_DATA.id
 }
 
 resource "azurerm_data_factory_linked_service_sql_server" "liked_sqlserver" {
   name            = var.linked_sql
-  data_factory_id = azurerm_data_factory.DV.id
+  data_factory_id = azurerm_data_factory.DF.id
   connection_string = "Integrated Security=False;Data Source=test;Initial Catalog=test;User ID=test;"
   
   key_vault_password {
--- case1/templates/data_factory/output.tf
+++ case1/templates/data_factory/output.tf
@@ -2,6 +2,6 @@ output "output_vault" {
   value = {}
 
   depends_on = [
-    azurerm_data_factory.nonprd-polaris-df-northeurope
+    azurerm_data_factory.DF
   ]
 }

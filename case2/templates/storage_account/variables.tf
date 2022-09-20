dd26d887fe289944cdc4c288ed471d8fc809aef5
Carlos Brito Leal <cbritole@emeal.nttdata.com>
2022-09-20 13:54:03 +0100

    1.0.1 - added templates of rg, sa

---
+++ case2/templates/storage_account/variables.tf
@@ -0,0 +1,19 @@
+variable "storage_account_name" {
+	type = string
+}
+
+variable "resource_group_name" {
+	type = string
+}
+
+variable "resource_group_location" {
+	type = string
+}
+
+variable "storage_account_tier" {
+	type = string
+}
+
+variable "storage_account_replication_type" {
+	type = string
+}
